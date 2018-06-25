aa67e4af78c38000c681b6b5e86e1f893361eb00
Alex Tu <alex.tu@canonical.com>
2018-06-25 14:19:08 +0800

    refine: support /dev/sda
    Signed-off-by: Alex Tu <alex.tu@canonical.com>
    
    # Merge-Proposal:
    # Bug:  which is polling by jenkins in launchpad MP to comment related bugs.
    # for commit message in launchpad: https://goo.gl/V9ePxQ
    # (LP: #xxxxxxx)

--- usr/sbin/rsync_iso.sh
+++ usr/sbin/rsync_iso.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
-#set -x
+set -x
 set -e
 CD_PATH="$PWD"
 usage() {
 cat << EOF
 usage: $0 options --iso [ISO file]
 
-** This script assume you only have one USB disk plugged, USB disk
-   will be wipe.
+** This script assume you only have one USB disk plugged, USB disk will be wipe.
 
 This command will create a bootable usb disk by the ISO file,
 It assume the iso file in the same folder where script be executed.
 options:
     -h|--help   print this message
     -f|--folder the folder you would like to inject fish tarballs
-                it assume the foler is in the same folder where
-                script be executed.
-    -i|--iso    the iso file which be used to create a bootable USB
-                disk.
+                it assume the foler is in the same folder where script be executed.
+    -i|--iso    the iso file which be used to create a bootable USB disk.
 example:
-    * create a USB which already be formated to FAT32 with foo.iso
-    iso file.
-    $ ./rsync_iso.sh --iso foo.iso
 
-    * create a USB which already be formated to FAT32 with foo.iso
-    iso file and also include the security fish tarball in folder
-    "bar_folder"
-    $ ./rsync_iso.sh --iso foo.iso --folder bar_folder
 EOF
 exit 1
 }
@@ -60,8 +50,18 @@ do
 done
 
 [ -z "$ISO" ] && usage
+if  mount | grep "on \/ " | grep nvme; then
+    echo nvme
+    usb_list=$(mount | grep -e "sd[a-z]" | grep media | cut -d ' ' -f3)
+else
+    echo sda
+    usb_list=$(mount | grep -e "sd[b-z]" | grep media | cut -d ' ' -f3)
+fi
+if [ ! -d "$usb_list" ]; then
+    echo "can not find any USB be insertted!"
+    usage
+fi
 
-usb_list=$(mount | grep -e "sd[b-z]" | cut -d ' ' -f3)
 mnt_folder=$(mktemp -d -p .)
 
 if [ $(echo $list | wc -l) -gt 1 ];then
@@ -71,7 +71,6 @@ if [ $(echo $list | wc -l) -gt 1 ];then
 fi
 
 target_folder="$usb_list"
-[ -d "$target_folder" ] || exit 1
 echo "press any key to wipe $target_folder and create bootable usb..... "
 read
 sudo mount -o loop $ISO $mnt_folder
