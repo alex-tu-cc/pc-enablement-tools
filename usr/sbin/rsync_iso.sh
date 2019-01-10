e2d99b9b91357c468e7bc0e153e0475b34d080cf
Alex Tu <alex.tu@canonical.com>
2019-01-10 11:42:56 +0800

    add rsync_iso.sh for personal usage

--- usr/sbin/rsync_iso.sh
+++ usr/sbin/rsync_iso.sh
@@ -1,79 +1,30 @@
 #!/bin/bash
 set -x
 set -e
-CD_PATH="$PWD"
-usage() {
-cat << EOF
-usage: $0 options --iso [ISO file]
+echo "usage: $0 \$target-iso-file.so"
 
-** This script assume you only have one USB disk plugged, USB disk will be wipe.
-
-This command will create a bootable usb disk by the ISO file,
-It assume the iso file in the same folder where script be executed.
-options:
-    -h|--help   print this message
-    -f|--folder the folder you would like to inject fish tarballs
-                it assume the foler is in the same folder where script be executed.
-    -i|--iso    the iso file which be used to create a bootable USB disk.
-example:
-
-EOF
-exit 1
-}
-while [ $# -gt 0 ]
-do
-    case "$1" in
-        -h | --help)
-            usage 0
-            exit 0
-            ;;
-        -f |--folder)
-            shift
-            FOLDER=$1;
-            if [ ! -d $FOLDER ];then
-                echo "not exists $FOLDER"
-                exit 1
-            fi
-            ;;
-        -i | --iso)
-            shift
-            ISO=$1
-            if [ ! -e "$ISO" ]; then
-                echo "not exists $FOLDER"
-                exit 1
-            fi
-            ;;
-        *)
-            usage
-       esac
-       shift
-done
-
-[ -z "$ISO" ] && usage
-if  mount | grep "on \/ " | grep nvme; then
-    echo nvme
-    usb_list=$(mount | grep -e "sd[a-z]" | grep media | cut -d ' ' -f3)
-else
-    echo sda
-    usb_list=$(mount | grep -e "sd[b-z]" | grep media | cut -d ' ' -f3)
-fi
-if [ ! -d "$usb_list" ]; then
-    echo "can not find any USB be insertted!"
-    usage
+if [ ! -e "$1" ]; then
+    echo "iso file $1 not exist"
+    exit 1
 fi
 
-mnt_folder=$(mktemp -d -p .)
+usb_dev=$(mount | grep -e "\/sd[c-z]" | awk -F'type|on' '{print $1}')
+#usb_dev="/dev/sda1"
+mnt_folder=$(mktemp -d)
 
-if [ $(echo $list | wc -l) -gt 1 ];then
+if [ $(echo $usb_dev | wc -l) -gt 1 ];then
     echo "there are more than one usb...."
     echo $list
     exit
 fi
 
-target_folder="$usb_list"
-echo "press any key to wipe $target_folder and create bootable usb..... "
-read
-sudo mount -o loop $ISO $mnt_folder
+target_folder=$(mktemp -d)
+sudo umount $usb_dev
+sudo mount -t vfat -o gid=$UID,uid=$UID $usb_dev $target_folder
+
+sudo mount -o loop $1 $mnt_folder
+#target_folder="$usb_list"
+[ -d "$(eval echo $target_folder)" ] || exit 1
 ionice -c 3 rsync --delete -avP $mnt_folder/ "$target_folder"
 #rm -rf "$target_folder"/*
 #ionice -c 3 msrsync -p 2 -P -r "-av" $mnt_folder/ "$target_folder"
@@ -84,14 +35,18 @@ echo ================================================================
 echo $iso_name
 echo ================================================================
 pushd "$target_folder"
+    #git clone https://github.com/alex-tu-cc/pc-tools-on-usb.git || true
+    cp -rf /home/alextu/my-library/github/pc-tools-on-usb .
     echo $project_name | tee project
-    if [ -n $FOLDER ]; then
-        for file in $(ls $CD_PATH/$FOLDER); do
-             [ -z ${file##*.gz}] && tar xvf $CD_PATH/$FOLDER/$file
-        done
-    fi
+
+    pushd "pc-tools-on-usb"
+        git remote add github https://github.com/alex-tu-cc/pc-tools-on-usb.git
+        cp -r fish/* ../
+    popd
+
 popd
 
 sudo umount $mnt_folder
+sudo umount $target_folder
 rm -rf $mnt_folder
-[ -e $(which zenity) ] && zenity --info --text="$0 $iso_name done"
+notify_local.sh "$0 $iso_name done"
